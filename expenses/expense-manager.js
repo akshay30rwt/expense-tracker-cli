3aa1d46cdfdcc99a62bb4d9f42d7a07c6c6bb8e2
akshay30rwt <akshayrwt8700@gmail.com>
2026-06-23 22:29:55 +0530

    Implement create, read, and delete operations

---
+++ expenses/expense-manager.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const FILE_PATH = './data.json';
+
+function readData() {
+    const fileContent = fs.readFileSync(FILE_PATH, 'utf-8');
+    return JSON.parse(fileContent);
+}
+
+function writeData(data) {
+    fs.writeFileSync(FILE_PATH, JSON.stringify(data, null, 2));
+}
+
+function addExpense(title, amount, category) {
+    const data = readData();
+
+    const maxId = data.expenses.length > 0 
+    ? Math.max(...data.expenses.forEach(expense => expense.id))
+    : 0
+
+    const newExpense = {
+        id: maxId + 1,
+        title: title,
+        amount: amount,
+        category: category
+    }
+    data.expenses.push(newExpense);
+    writeData(data);
+    console.log(`Expense: '${title}' added successfully`);
+}
+
+function viewExpenses() {
+    const data = readData();
+    if(data.expenses.length === 0) {
+        console.log('No expense found');
+        return;
+    }
+
+    data.expense.forEach(expense => {
+        console.log(`ID: ${expense.id} | Title: ${expense.title} | Amount: ${expense.amount} | Category: ${expense.category}`);
+    });
+}
+
+function deleteExpense(id) {
+    const data = readData();
+    const index = data.expenses.findIndex(expense => expense.id === id);
+
+    if(index === -1) {
+        console.log(`Expense with ID: ${id} not found`);
+        return;
+    }
+    const deletedExpense = data.expenses[index];
+    data.expenses.splice(index, 1);
+    console.log(`Expense: '${deletedExpense.title}' deleted successfully`);
+}
+
+function totalSpent() {
+    const data = readData();
+}
+
+module.exports = { addExpense, viewExpenses, deleteExpense, totalSpent };
